2e8f3c256b736ce20172fd36760547cb448152ed
adamwm4 <adamwm4@illinois.edu>
2025-03-24 16:35:01 -0500

    the basic web-rtc communication works

--- web-rtc/server.js
+++ web-rtc/server.js
@@ -2,27 +2,18 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-let clients = [];
-
 wss.on('connection', (ws) => {
-	clients.push(ws);
-	console.log("New client connected");
-
+	console.log("connected")
 	ws.on('message', (message) => {
-		console.log("Received:", message);
-
-		// Forward message to the other peer
-		clients.forEach(client => {
+		console.log("recieved message:", message.toString())
+		// Broadcast message to all connected clients except sender
+		wss.clients.forEach(client => {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(message);
+
+				client.send(message.toString());
 			}
 		});
 	});
-
-	ws.on('close', () => {
-		clients = clients.filter(client => client !== ws);
-		console.log("Client disconnected");
-	});
 });
 
-console.log("WebSocket signaling server running on ws://localhost:8080");
+console.log("WebSocket server running on ws://localhost:8080");
