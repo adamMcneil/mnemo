4b3b86ee7f27dffe5bc91a8d7a1da9da3c7f19c4
adamwm4 <adamwm4@illinois.edu>
2025-03-28 15:30:48 -0500

    added number of messages processed to websocket

---
+++ web-socket/graph.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = 100
+# Read the file and parse timestamps
+recv = pd.read_csv('recieved-1743193554388.log', names=['time'])
+sent = pd.read_csv('sent-1743193554388.log', names=['time'])
+
+# Group by second and count messages
+recv = recv['time'].astype(int)
+sent = sent['time'].astype(int)
+min = recv.min()
+recv = recv.sub(min)
+sent = sent.sub(min)
+min = recv.min()
+max = sent.max()
+
+recv_group = pd.cut(recv, bins=range(min, max+x, x), right=False)
+sent_group = pd.cut(sent, bins=range(min, max+x, x), right=False)
+
+recv_counts = pd.value_counts(recv_group).sort_index()
+recv_labels = [interval.left + (x/2) for interval in recv_counts.index]
+
+sent_counts = pd.value_counts(sent_group).sort_index()
+sent_labels = [interval.left + (x/2) for interval in sent_counts.index]
+
+plt.plot(recv_labels, recv_counts.values, marker='o', linestyle='-')
+plt.plot(sent_labels, sent_counts.values, marker='o', linestyle='-')
+
+plt.xlabel("Time Ranges (Midpoints)")
+plt.ylabel("Number of Messages")
+plt.title("Distribution of Times Grouped by 100s")
+plt.ylim(bottom=0)
+plt.grid(True)
+plt.show()
+
--- web-socket/server.js
+++ web-socket/server.js
@@ -33,16 +33,29 @@ const server = http.createServer((req, res) => {
   });
 });
 
+
+const recieved = `recieved-${Date.now()}.log`;
+const sent = `sent-${Date.now()}.log`;
+function writeTime(filePath) {
+  fs.writeFile(filePath, `${Date.now()}\n`, { flag: 'a' }, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    }
+  });
+}
+
 wssServer.on('connection', function connection(wsServer) {
   wss.on('connection', function connection(ws, req) {
     const clientIP = req.socket.remoteAddress;
     console.log(`Client connected: ${clientIP}`);
 
     ws.on('message', function incoming(message) {
+      writeTime(recieved);
       const messageObject = JSON.parse(message);
       messageObject.action = "update";
       messageObject.ip = clientIP;
       wsServer.send(JSON.stringify(messageObject));
+      writeTime(sent);
       controllerStates[messageObject.name] = messageObject;
     });
 
