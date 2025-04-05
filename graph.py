af493cd08fad1da022eb92484265752eaa40c8b3
adamwm4 <adamwm4@illinois.edu>
2025-04-05 09:58:26 -0500

    hundred clients

--- graph.py
+++ graph.py
@@ -46,8 +46,8 @@ def plot_socket(recv_name, sent_name):
     plt.plot(recv_labels, recv_counts.values, marker='o', linestyle='-', label='WebSocket')
     # plt.plot(sent_labels, sent_counts.values, marker='o', linestyle='-')
 
-plot_socket('data/fifty-client/recieved.data', 'data/fifty-client/sent.data')
-plot_rtc('data/fifty-client/recv.data')
+plot_socket('data/hundred-client/web-socket-100-clients.data', 'data/hundred-client/web-socket-100-clients.data')
+plot_rtc('data/hundred-client/web-rtc-100-client.data')
 plt.xlabel("Time (1/10s)")
 plt.ylabel("Number of Messages")
 plt.title("Number of Messages vs. Time (1/10s)")
