b75d8f0250b112ae9d8db3031ec348d6b1ebaf4d
adamwm4 <adamwm4@illinois.edu>
2025-05-08 13:56:35 -0500

    added artillery

--- http/client.js
+++ http/client.js
@@ -7,27 +7,28 @@ const options = {
 	path: '/api/movement',
 	method: 'POST',
 };
-
-const numberOfMessages = 10000;
+console.log(process.argv)
+const numberOfMessages = parseInt(process.argv[2]);
 
 function timeMessages() {
-	let startTime = performance.now();
 	for (let i = 0; i < numberOfMessages; i++) {
 		sendPost();
 	}
-	let endTime = performance.now();
-	console.log(startTime)
-	let latency = endTime - startTime;
-	console.log(`Latency: ${latency.toFixed(2)} ms`);
 }
 
 function sendPost() {
+	let startTime = performance.now();
 	const req = http.request(options, (res) => {
 		res.on('data', () => {
 			// Consume response data to ensure 'end' event is triggered
 		});
 
 		res.on('end', () => {
+			let endTime = performance.now();
+			let latency = endTime - startTime;
+			// console.log(`Latency: ${latency.toFixed(2)} ms`);
+			// console.log(`Latency: ${latency / numberOfMessages.toFixed(2)} ms`);
+			return latency;
 		});
 	});
 
