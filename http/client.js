d3046ee6962b3391d111f0e2793f096a173a6525
adamwm4 <adamwm4@illinois.edu>
2025-05-08 13:17:50 -0500

    added multiple messages

--- http/client.js
+++ http/client.js
@@ -2,28 +2,40 @@ const http = require('http');
 const { performance } = require('perf_hooks');
 
 const options = {
-  hostname: 'localhost',
-  port: 3000,
-  path: '/api/movement',
-  method: 'POST',
+	hostname: 'localhost',
+	port: 3000,
+	path: '/api/movement',
+	method: 'POST',
 };
 
-const startTime = performance.now();
+const numberOfMessages = 10000;
 
-const req = http.request(options, (res) => {
-  res.on('data', () => {
-    // Consume response data to ensure 'end' event is triggered
-  });
+function timeMessages() {
+	let startTime = performance.now();
+	for (let i = 0; i < numberOfMessages; i++) {
+		sendPost();
+	}
+	let endTime = performance.now();
+	console.log(startTime)
+	let latency = endTime - startTime;
+	console.log(`Latency: ${latency.toFixed(2)} ms`);
+}
 
-  res.on('end', () => {
-    const endTime = performance.now();
-    const latency = endTime - startTime;
-    console.log(`Latency: ${latency.toFixed(2)} ms`);
-  });
-});
+function sendPost() {
+	const req = http.request(options, (res) => {
+		res.on('data', () => {
+			// Consume response data to ensure 'end' event is triggered
+		});
 
-req.on('error', (error) => {
-  console.error(`Error: ${error.message}`);
-});
+		res.on('end', () => {
+		});
+	});
 
-req.end();
+	req.on('error', (error) => {
+		console.error(`Error: ${error.message}`);
+	});
+
+	req.end();
+}
+
+timeMessages()
