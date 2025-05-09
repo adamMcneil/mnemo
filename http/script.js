3877c8e167231b6392812b762de72386cdc4963c
adamMcneil <adamwm4@sp25-cs525-0401.cs.illinois.edu>
2025-05-09 09:38:10 -0500

    added data from vm

--- http/script.js
+++ http/script.js
@@ -9,7 +9,7 @@ export let options = {
 };
 
 export default function() {
-	http.post('http://localhost:3000/api/movement');
+	http.post('https://6be5-192-241-60-98.ngrok-free.app:3000/api/movement');
 	sleep(1.0 / 60.0);
 }
 
