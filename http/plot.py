6f67283be45c0b6f16ab92c3b6e361fd5b286386
adamwm4 <adamwm4@illinois.edu>
2025-05-10 09:39:59 -0500

    combined plots to make them clearer

--- http/plot.py
+++ http/plot.py
@@ -17,10 +17,20 @@ cols = math.ceil(math.sqrt(num_files))
 rows = math.ceil(num_files / cols)
 
 sns.set(style="whitegrid")
+sns.set_context("notebook", font_scale=1.25)
 fig, axes = plt.subplots(rows, cols, figsize=(cols * 6, rows * 4))
 axes = axes.flatten()  # Flatten to 1D array for easy indexing
 
-for i, filename in enumerate(json_files):
+file_list = [
+    "ws_global.json",
+    "ws_lan.json",
+    "ws_machine.json",
+    "http_global.json",
+    "http_lan.json",
+    "http_machine.json",
+]
+
+for i, filename in enumerate(file_list):
     filepath = os.path.join(directory, filename)
     print(filepath)
     with open(filepath) as f:
@@ -32,13 +42,27 @@ for i, filename in enumerate(json_files):
     p95_durations = [entry['latency']['p95'] for entry in data]
     # max_durations = [entry['latency']['max'] for entry in data]
 
-    ax = axes[i]
-    ax.plot(vus, avg_durations, marker='o', label='Avg')
-    ax.plot(vus, p95_durations, marker='x', label='P95')
-    # ax.plot(vus, max_durations, marker='s', label='Max')
+    ax = axes[i%3]
+    http = True
+    if (i < 3):
+        http = False
 
-    ax.set_title(filename)
-    ax.set_xlabel("VUs")
+    if http:
+        ax.plot(vus, avg_durations, marker='o', label='Avg http', color='red')
+        ax.plot(vus, p95_durations, marker='x', label='P95 http', color='red')
+    else:
+        ax.plot(vus, avg_durations, marker='.', label='Avg ws', color='blue')
+        ax.plot(vus, p95_durations, marker='X', label='P95 ws', color='blue')
+
+    # ax.plot(vus, max_durations, marker='s', label='Max')
+    if (i == 0):
+        ax.set_title(filename)
+    elif (i == 1):
+        ax.set_title("Latency vs VUs connected with LAN")
+    elif (i == 2):
+        ax.set_title("Latency vs VUs on the same machine")
+
+    ax.set_xlabel("# of VUs")
     ax.set_ylabel("Latency (ms)")
     ax.legend()
     ax.grid(True)
